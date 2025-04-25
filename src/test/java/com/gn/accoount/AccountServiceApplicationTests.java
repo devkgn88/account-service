5996c094296a7cda7c9d9da392062f5a1783432b
devkgn88 <devkgn88@gmail.com>
2025-04-25 12:19:24 +0900

    [김가남] gradle 설정 및 환경설정 파일 ignore처리

---
+++ src/test/java/com/gn/accoount/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gn.accoount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
